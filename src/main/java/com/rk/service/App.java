b333ca7801d8153d85f5fa554bcb72e3f11b77fc
Rounak Harde <rounakharde86@gmail.com>
2024-09-24 15:06:02 +0530

    1st project is commit

---
+++ src/main/java/com/rk/service/App.java
@@ -0,0 +1,13 @@
+package com.rk.service;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a=200,b=900;
+    	
+        System.out.println( "Hello Spring Boot !!!" );
+        System.out.println(a+b);
+        
+    }
+}
