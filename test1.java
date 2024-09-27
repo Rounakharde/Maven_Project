88b8dd5815e4d8d9e418c199a72da50f149342a6
rounak <rounakharde@gmail.com>
2024-09-28 01:00:21 +0530

    modified test and test1 by TL

---
+++ test1.java
@@ -0,0 +1,12 @@
+class Test1
+{
+    public void m(){
+        System.out.println("Method m")
+    }
+    public static void main(String[] args) {
+
+        Test1 t=new Test1();
+        t.m();
+        
+    }
+}
